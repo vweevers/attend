6d8f9b4011b204e57f6adcdca63b3dcac75f7033
Vincent Weevers <mail@vincentweevers.nl>
2020-11-22 09:44:08 +0100

    Refactor attend-npm-dependencies
    Fixes various issues:
    
    - Support fixing dependencies if `node_modules` doesn't exist. In
      this case we directly modify `package.json` without spawning npm.
      This allows fast maintenance of many projects (even more so when
      using a sparse checkout).
    - If `node_modules` does exist then warn/error about both missing
      and outdated dependencies. I.e. one dependency may result in
      multiple messages.
    - Collect all information (messages and fixes) before taking action,
      so that we can potentially reuse that information elsewhere (like
      in attend-dependabot, to ignore certain dependencies).
    - Don't bump deps with incompatible `engines.node`.
    - Don't bump if latest tagged version (e.g. 1.0.0) is less than
      current prerelease (2.0.0-rc.1).
    - When fixing a missing dependency and we don't want to bump, then
      install the version specified in package.json	rather than the
      latest version.

--- vendor/attend-npm-dependencies/index.js
+++ vendor/attend-npm-dependencies/index.js
@@ -2,18 +2,27 @@
 
 const vfile = require('vfile')
 const jsonMap = require('json-source-map')
+const semverSatisfies = require('semver/functions/satisfies')
+const semverMinVersion = require('semver/ranges/min-version')
+const semverGt = require('semver/functions/gt')
+const promisify = require('util').promisify
+const memoize = require('thunky-with-args')
+const packument = memoize(require('packument').factory({ keepAlive: true }))
+const getPackage = promisify(require('packument-package').factory(packument))
 const npmCheck = require('npm-check')
 const EventEmitter = require('events')
 const spawn = require('child_process').spawn
 const path = require('path')
 const fs = require('fs')
+const fsp = fs.promises
 const hasOwnProperty = Object.prototype.hasOwnProperty
 
 module.exports = function (pluginOptions) {
   pluginOptions = pluginOptions || {}
 
   const plugin = new EventEmitter()
-  const include = packagefilter(pluginOptions)
+  const only = pluginOptions.only
+  const ignore = pluginOptions.ignore
   const bump = !!pluginOptions.bump
 
   plugin.init = async function (options) {
@@ -27,52 +36,54 @@ module.exports = function (pluginOptions) {
   plugin.lint = async function (options) {
     const cwd = path.resolve(options.cwd || '.')
     const bare = !fs.existsSync(path.join(cwd, 'node_modules'))
-    const lazyMessages = new LazyMessages(cwd, include, bump, bare)
-    const currentState = await npmCheck({ cwd, skipUnused: true })
-    const packages = currentState.get('packages')
+    const packages = await check({ cwd, bump, bare, only, ignore })
 
-    for (const item of packages) {
-      lazyMessages.lintItem(item)
-    }
-
-    return { files: [lazyMessages.finalize()] }
+    return { files: [finalize(cwd, packages)] }
   }
 
   plugin.fix = async function (options) {
     const cwd = path.resolve(options.cwd || '.')
     const bare = !fs.existsSync(path.join(cwd, 'node_modules'))
-    const lazyMessages = new LazyMessages(cwd, include, bump, bare)
+    const remove = { prod: [], dev: [] }
+    const install = { prod: { '^': [], '~': [], '': [] }, dev: { '^': [], '~': [], '': [] } }
+    const packages = await check({ cwd, bump, bare, only, ignore })
 
-    // TODO: in bare mode, update package.json directly without npm
-    if (bare) {
-      await npm(cwd, ['i'], null, plugin)
-    }
-
-    const currentState = await npmCheck({ cwd, skipUnused: true })
-    const packages = currentState.get('packages')
-
-    const remove = {
-      prod: [],
-      dev: []
-    }
-
-    const update = {
-      prod: { caret: [], tilde: [], default: [] },
-      dev: { caret: [], tilde: [], default: [] }
-    }
+    // In bare mode, update package.json directly without npm
+    const newPkg = bare ? readPackage(cwd) : null
 
     for (const item of packages) {
-      if (!include(item) || item.regError) {
-        lazyMessages.lintItem(item)
-      } else if (item.unused) {
-        remove[item.devDependency ? 'dev' : 'prod'].push(item.moduleName)
-      } else if (fixable(item, bump, bare)) {
-        const prefix = item.packageJson[0]
-        const group = item.devDependency ? 'dev' : 'prod'
-        const type = prefix === '^' ? 'caret' : prefix === '~' ? 'tilde' : 'default'
+      const id = item.moduleName
 
-        update[group][type].push(item.moduleName + '@' + item.latest)
+      if (item.fix && item.fix.action === 'remove') {
+        item.messages = item.messages.filter(unfixable)
+
+        if (newPkg) {
+          if (newPkg.dependencies) newPkg.dependencies[id] = undefined
+          if (newPkg.devDependencies) newPkg.devDependencies[id] = undefined
+          if (newPkg.optionalDependencies) newPkg.optionalDependencies[id] = undefined
+        } else {
+          remove[item.devDependency ? 'dev' : 'prod'].push(id)
+        }
+      } else if (item.fix && item.fix.action === 'install' && item.fix.version) {
+        item.messages = item.messages.filter(unfixable)
+
+        if (newPkg) {
+          const group = newPkg[item.devDependency ? 'devDependencies' : 'dependencies']
+          const spec = (item.fix.savePrefix || '^') + item.fix.version
+
+          group[id] = spec
+        } else {
+          const group = install[item.devDependency ? 'dev' : 'prod']
+          const spec = id + '@' + item.fix.version
+
+          group[item.fix.savePrefix || ''].push(spec)
+        }
       }
+    }
+
+    if (newPkg) {
+      const json = JSON.stringify(newPkg, null, 2) + '\n'
+      await fsp.writeFile(path.join(cwd, 'package.json'), json)
     }
 
     for (const group of ['prod', 'dev']) {
@@ -82,34 +93,19 @@ module.exports = function (pluginOptions) {
         await npm(cwd, ['rm', ...remove[group], save], null, plugin)
       }
 
-      if (update[group].caret.length) {
-        await npm(cwd, ['i', ...update[group].caret, save], { npm_config_save_prefix: '^' }, plugin)
-      }
-
-      if (update[group].tilde.length) {
-        await npm(cwd, ['i', ...update[group].tilde, save], { npm_config_save_prefix: '~' }, plugin)
-      }
-
-      if (update[group].default.length) {
-        await npm(cwd, ['i', ...update[group].default, save], null, plugin)
+      for (const savePrefix of ['^', '~', '']) {
+        if (install[group][savePrefix].length) {
+          const env = savePrefix ? { npm_config_save_prefix: savePrefix } : null
+          await npm(cwd, ['i', ...install[group][savePrefix], save], env, plugin)
+        }
       }
     }
 
     // Get message positions *after* updating package.json
-    return { files: [lazyMessages.finalize()] }
+    return { files: [finalize(cwd, packages)] }
   }
 
   return plugin
-}
-
-function fixable (item, bump, bare) {
-  return (
-    (!bare && item.mismatch) ||
-    (!bare && item.pkgError) ||
-    (!bare && !item.semverValid) ||
-    (!bare && item.notInstalled) ||
-    (bump && item.packageWanted !== item.latest)
-  )
 }
 
 function tryRead (fp) {
@@ -120,84 +116,170 @@ function tryRead (fp) {
   }
 }
 
-class LazyMessages {
-  constructor (cwd, include, bump, bare) {
-    this.cwd = cwd
-    this.include = include
-    this.bump = bump
-    this.bare = bare
-    this.queue = []
-  }
+// TODO (refactor): move out, to be used by other attend
+// plugins (having cached the results in memory)
+async function check ({ cwd, bump, bare, only, ignore }) {
+  cwd = path.resolve(cwd || '.')
 
-  info (item, msg, rule) {
-    this.queue.push({ item, msg, rule, level: 'info' })
-  }
+  // Don't check for unused deps, too many false positives
+  const currentState = await npmCheck({ cwd, skipUnused: true })
+  const include = packagefilter(only, ignore)
+  const packages = currentState.get('packages').filter(include)
+  const pkg = readPackage(cwd)
 
-  warn (item, msg, rule) {
-    this.queue.push({ item, msg, rule, level: 'warn' })
-  }
+  return Promise.all(packages.map(async function (item) {
+    const id = item.moduleName
+    const messages = item.messages = []
 
-  fatal (item, msg, rule) {
-    this.queue.push({ item, msg, rule, level: 'fatal' })
-  }
-
-  lintItem (item) {
-    const { moduleName: id, devDependency: dev } = item
+    item.outdated = false
+    item.ignoreUpdates = false
+    item.fix = null
 
     if (item.regError) {
-      this.fatal(item, error(item.regError), 'no-registry-error')
-    } else if (item.unused) {
-      this.warn(item, `Remove unused dependency \`${id}\``, 'no-unused')
-    } else if (item.notInstalled && !this.bare) {
-      this.fatal(item, `Install missing dependency \`${id}\``, 'no-missing')
-    } else if (item.pkgError && !this.bare) {
-      this.fatal(item, error(item.pkgError), 'valid-package')
-    } else if (item.mismatch && !this.bare) {
-      this.fatal(item, `Wants \`${id} ${item.packageWanted}\`, has \`${item.installed}\``, 'no-mismatch')
-    } else if (!item.semverValid && !this.bare) {
-      this.fatal(item, `Fix invalid version: \`${id} ${item.installed}\``, 'valid-version')
-    } else if (this.bump && item.packageWanted !== item.latest) {
-      if (/^\d/.test(item.packageJson) && !this.include.only(item)) {
+      messages.push({
+        fatal: true,
+        fixable: false,
+        reason: error(item.regError),
+        ruleId: 'no-registry-error'
+      })
+      return item
+    }
+
+    if (item.unused) {
+      item.ignoreUpdates = true
+      item.fix = { action: 'remove' }
+      messages.push({
+        fatal: false,
+        fixable: true,
+        reason: `Remove unused dependency \`${id}\``,
+        ruleId: 'no-unused'
+      })
+      return item
+    }
+
+    if (bump && semverGt(item.latest, item.packageWanted)) {
+      const explicitlyIncluded = include.only(item)
+
+      if (!explicitlyIncluded && /^\d/.test(item.packageJson)) {
         // Probably pinned for a reason. Only warn
-        const msg = `Bump pinned \`${id}\` from \`${item.packageWanted}\` to \`${item.latest}\``
-        this.warn(item, msg, 'bump')
+        item.outdated = true
+        item.ignoreUpdates = true
+        messages.push({
+          fatal: false,
+          fixable: false,
+          reason: `Skip bumping pinned \`${id}\` from \`${item.packageWanted}\` to \`${item.latest}\``,
+          ruleId: 'bump'
+        })
+      } else if (!explicitlyIncluded && !(await compatible(pkg, item))) {
+        item.outdated = true
+        item.ignoreUpdates = true
+        messages.push({
+          fatal: false,
+          fixable: false,
+          reason: `Skip bumping incompatible \`${id}\` to \`${item.latest}\``,
+          ruleId: 'bump'
+        })
       } else {
-        const msg = `Bump \`${id}\` from \`${item.packageWanted}\` to \`${item.latest}\``
-        this.fatal(item, msg, 'bump')
+        item.outdated = true
+        item.fix = updateFix(item)
+        messages.push({
+          fatal: true,
+          fixable: true,
+          reason: `Bump \`${id}\` from \`${item.packageWanted}\` to \`${item.latest}\``,
+          ruleId: 'bump'
+        })
       }
     }
-  }
 
-  finalize () {
-    const cwd = this.cwd
-    const contents = tryRead(path.join(cwd, 'package.json'))
-    const packageFile = vfile({ path: 'package.json', cwd, contents })
-    const position = contents ? positions(contents) : () => null
-
-    for (const { item, msg, rule, level } of this.queue) {
-      const pos = item ? position(item.moduleName, item.devDependency) : null
-      const origin = rule ? `attend-npm-dependencies:${rule}` : null
-
-      if (level !== 'info' && (!item || this.include(item))) {
-        const message = packageFile.message(msg, pos, origin)
-
-        if (level === 'fatal') message.fatal = true
-      } else {
-        // packageFile.info(`(${msg})`, pos, origin)
+    if (!bare) {
+      if (item.notInstalled) {
+        // An existing fix from above (with a new version) takes precedence
+        item.fix = item.fix || installFix(item)
+        messages.push({
+          fatal: true,
+          fixable: true,
+          reason: `Install missing dependency \`${id}\``,
+          ruleId: 'no-missing'
+        })
+      } else if (item.pkgError) {
+        item.fix = item.fix || installFix(item)
+        messages.push({
+          fatal: true,
+          fixable: true,
+          reason: error(item.pkgError),
+          ruleId: 'valid-package'
+        })
+      } else if (item.mismatch) {
+        item.fix = item.fix || installFix(item)
+        messages.push({
+          fatal: true,
+          fixable: true,
+          reason: `Wants \`${id} ${item.packageWanted}\`, has \`${item.installed}\``,
+          ruleId:  'no-mismatch'
+        })
+      } else if (!item.semverValid) {
+        item.fix = item.fix || installFix(item)
+        messages.push({
+          fatal: true,
+          fixable: true,
+          reason: `Fix invalid version: \`${id} ${item.installed}\``,
+          ruleId: 'valid-version'
+        })
       }
     }
 
-    return packageFile
-  }
+    return item
+  }))
 }
 
+function updateFix (item) {
+  return getFix(item, item.latest)
+}
+
+function installFix (item) {
+  return getFix(item, item.packageWanted)
+}
+
+function getFix (item, version) {
+  // Honor existing prefix
+  const char = item.packageJson[0]
+  const savePrefix = char === '^' || char === '~' ? char : null
+
+  return { action: 'install', version, savePrefix }
+}
+
+function finalize (cwd, packages) {
+  const contents = tryRead(path.join(cwd, 'package.json'))
+  const packageFile = vfile({ path: 'package.json', cwd, contents })
+  const position = contents ? positions(contents) : () => null
+
+  for (const item of packages) {
+    if (item.messages.length === 0) {
+      continue
+    }
+
+    const pos = position(item.moduleName, item.devDependency)
+
+    for (const { reason, ruleId, fatal } of item.messages) {
+      const origin = ruleId ? `attend-npm-dependencies:${ruleId}` : null
+      const msg = packageFile.message(reason, pos, origin)
+
+      if (fatal) msg.fatal = true
+    }
+  }
+
+  return packageFile
+}
+
+// TODO: vfile-message has a .stack property,
+// use that here and in vfile-reporter-shiny
 function error (err) {
   return String(err.stack || err)
 }
 
-function packagefilter (pluginOptions) {
-  const only = [].concat(pluginOptions.only || [])
-  const ignore = [].concat(pluginOptions.ignore || [])
+function packagefilter (only, ignore) {
+  only = [].concat(only || [])
+  ignore = [].concat(ignore || [])
 
   // Check if a dependency should be included
   const include = function (item) {
@@ -213,6 +295,24 @@ function packagefilter (pluginOptions) {
   }
 
   return include
+}
+
+function unfixable (message) {
+  return !message.fixable
+}
+
+// TODO: use pacote or npm-registry-fetch to make use of npm's cache
+async function compatible (ourPkg, item) {
+  const ourEngine = ourPkg.engines && ourPkg.engines.node
+
+  if (!ourEngine) return true
+
+  const theirPkg = await getPackage(item.moduleName, item.latest)
+  const theirEngine = theirPkg.engines && theirPkg.engines.node
+
+  if (!theirEngine) return true
+
+  return semverSatisfies(semverMinVersion(ourEngine), theirEngine)
 }
 
 function npm (cwd, args, env, ee) {
@@ -267,4 +367,11 @@ function fixPosition (pos) {
   }
 
   return pos
+}
+
+function readPackage (cwd) {
+  const fp = path.join(cwd, 'package.json')
+  const json = fs.readFileSync(fp, 'utf8')
+
+  return JSON.parse(json)
 }
